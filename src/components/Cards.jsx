57aa63ed8d2bda0634a286785573037ca7971ce9
asghar002345 <164898597+asghar002345@users.noreply.github.com>
2024-04-17 15:07:43 +0500

    homepage video update
    homepage video update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -20,7 +20,7 @@ const Cards = () => {
   }, []);
 
   return (
-    <div className="flex items-center font-poppins">
+    <div className="flex items-center justify-center font-poppins">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 sm:gap-16 items-end text-white sm:h-[400px] md:h-[450px] mx-auto">
         <div className="flex flex-col justify-center items-center w-64 my-1 sm:my-3 rounded-lg h-32 sm:h-36 bg-[#040F1C] bg-opacity-90">
           <div className="flex justify-center border border-[#40576A] rounded-lg bg-[#354D61] w-10 h-10 mt-3 mb-2">
